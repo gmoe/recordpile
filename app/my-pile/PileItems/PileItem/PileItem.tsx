3a431e80a55271fef6db0d9fa3c824ed84ae90ba
Griffin Moe <me@griffinmoe.com>
2025-09-14 13:14:13 -0500

    Remove dnd-kit
    I'm unimpressed with that library. The boilerplate is ridiculous and
    it doesn't work on mobile despite using DragOverlay. Why did all of the
    good DnD libraries get deprecated? Going to pivot to manual sorting
    controls.

--- app/my-pile/PileItems/PileItem/PileItem.tsx
+++ app/my-pile/PileItems/PileItem/PileItem.tsx
@@ -1,11 +1,14 @@
 'use client';
 import { format } from 'date-fns';
-import { useSortable } from '@dnd-kit/sortable';
-import { CSS } from '@dnd-kit/utilities';
 
 import { PileItemStatus, PileItemStatusLabels } from '@/app/models/PileItemTypes';
 import Select from '@/app/components/Select';
-import { ClientPileItem, updatePileItem, deletePileItem } from '../../actions';
+import {
+  ClientPileItem,
+  updatePileItem,
+  deletePileItem,
+  reorderPileItem,
+} from '../../actions';
 import missingArt from './missingArt.svg';
 import styles from './PileItem.module.scss';
 
@@ -14,25 +17,9 @@ type PileItemProps = {
 };
 
 export default function PileItem({ item }: PileItemProps) {
-  const {
-    attributes,
-    listeners,
-    setNodeRef,
-    transform,
-    transition,
-    active,
-  } = useSortable({ id: item.id });
-
-  const style = {
-    transform: CSS.Transform.toString(transform),
-    transition,
-    cursor: active ? 'grabbing' : 'grab',
-  };
-
   return (
-    <li className={styles.item} ref={setNodeRef} style={style} {...attributes} >
+    <li className={styles.item}>
       <img
-        {...listeners}
         src={item.coverImageUrl}
         alt={`Album art for ${item.albumName}`}
         loading="lazy"
@@ -40,7 +27,7 @@ export default function PileItem({ item }: PileItemProps) {
           (event.target as HTMLImageElement).src = missingArt.src;
         }}
       />
-      <div className={styles.albumInfo} {...listeners}>
+      <div className={styles.albumInfo}>
         <span className={styles.artist}>{item.artistName}</span>
         <span className={styles.album}>{item.albumName}</span>
       </div>
--- app/my-pile/PileItems/PileItems.tsx
+++ app/my-pile/PileItems/PileItems.tsx
@@ -1,20 +1,4 @@
-'use client';
-import { useCallback, useMemo } from 'react';
-import {
-  DndContext,
-  DragEndEvent,
-  closestCenter,
-  KeyboardSensor,
-  PointerSensor,
-  useSensor,
-  useSensors,
-} from '@dnd-kit/core';
-import {
-  SortableContext,
-  sortableKeyboardCoordinates,
-  verticalListSortingStrategy,
-} from '@dnd-kit/sortable';
-import { ClientPileItem, reorderPileItem } from '../actions';
+import { ClientPileItem } from '../actions';
 import PileItem from './PileItem';
 import styles from './PileItems.module.scss';
 
@@ -23,40 +7,11 @@ interface PileItemsProps {
 }
 
 export default function PileItems({ pileItems }: PileItemsProps) {
-  const itemIds = useMemo(() => pileItems.reverse().map((item) => item.id), [pileItems]);
-
-  const sensors = useSensors(
-    useSensor(PointerSensor),
-    useSensor(KeyboardSensor, {
-      coordinateGetter: sortableKeyboardCoordinates,
-    })
-  );
-
-  const handleDragEng = useCallback((event: DragEndEvent) => {
-    const { active, over } = event;
-
-    if (active && over?.data?.current && active.id !== over.id) {
-      const target = pileItems.find((item) => item.id === over.id);
-      reorderPileItem(active.id as string, target!.orderIndex);
-    }
-  }, [itemIds, pileItems]);
-
   return (
-    <DndContext
-      sensors={sensors}
-      onDragEnd={handleDragEng}
-      collisionDetection={closestCenter}
-    >
-      <SortableContext
-        items={itemIds}
-        strategy={verticalListSortingStrategy}
-      >
-        <ol className={styles.pile}>
-          {pileItems.map((item) => (
-            <PileItem key={item.id} item={item} />
-          ))}
-        </ol>
-      </SortableContext>
-    </DndContext>
+    <ol className={styles.pile}>
+      {pileItems.map((item) => (
+        <PileItem key={item.id} item={item} />
+      ))}
+    </ol>
   );
 }
