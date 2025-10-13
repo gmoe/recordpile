08f3773358a8ca1e04459fb0452464db4f9c67dc
Griffin Moe <me@griffinmoe.com>
2025-10-13 15:42:40 -0500

    Add CA cert

--- app/db/database.ts
+++ app/db/database.ts
@@ -6,7 +6,9 @@ const isProduction = process.env.NODE_ENV !== 'development';
 export const database: ReturnType<typeof drizzle<typeof schema>> = drizzle({
   connection: {
     connectionString: process.env.DATABASE_URL!,
-    ssl: isProduction,
+    ssl: isProduction
+      ? { ca: process.env.DATABASE_CA_CERT! }
+      : false,
   },
   schema,
 });
