2995ab7b6abb2f83ddf7e9f73a65b241ef0b4c88
Griffin Moe <me@griffinmoe.com>
2025-10-13 18:12:50 -0500

    Add cert to config

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,15 @@
 import { defineConfig } from 'drizzle-kit';
 
+const isProduction = process.env.NODE_ENV !== 'development';
+
 export default defineConfig({
   dialect: 'postgresql',
   schema: './app/new-db/schemas',
   out: './drizzle',
   dbCredentials: {
     url: process.env.DATABASE_URL!,
+    ssl: isProduction
+      ? { ca: process.env.DATABASE_CA_CERT! }
+      : false,
   },
 })
