362bb3d835d391b8a789e9a5bdfccbb70d1dfb2a
Griffin Moe <me@griffinmoe.com>
2025-10-13 15:48:44 -0500

    Another CA fix

--- app/db/database.ts
+++ app/db/database.ts
@@ -1,4 +1,6 @@
+import { readFileSync } from 'node:fs';
 import { drizzle } from 'drizzle-orm/node-postgres';
+
 import * as schema from './schemas';
 
 const isProduction = process.env.NODE_ENV !== 'development';
@@ -7,7 +9,7 @@ export const database: ReturnType<typeof drizzle<typeof schema>> = drizzle({
   connection: {
     connectionString: process.env.DATABASE_URL!,
     ssl: isProduction
-      ? { ca: process.env.DATABASE_CA_CERT! }
+      ? { ca: readFileSync(process.env.DATABASE_CA_CERT!).toString() }
       : false,
   },
   schema,
