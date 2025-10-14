7842c8b4bd8e308b3d59864b591d9f1a3e80e6e1
Griffin Moe <me@griffinmoe.com>
2025-10-13 20:27:14 -0500

    Migrate from here?

---
+++ app/instrumentation.ts
@@ -0,0 +1,12 @@
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
+
+import { database } from '@/app/db';
+
+export async function register() {
+  if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NODE_ENV === 'production') {
+    await migrate(database, {
+      migrationsFolder: './drizzle',
+      migrationsSchema: 'public',
+    });
+  }
+}
