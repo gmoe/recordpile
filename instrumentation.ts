45a1fd6fad869a8d1f79074937838e08e931b89d
Griffin Moe <me@griffinmoe.com>
2025-10-13 20:52:31 -0500

    Migration tweaks

--- instrumentation.ts
+++ instrumentation.ts
@@ -1,15 +1,11 @@
-import { resolve } from 'node:path';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 
 import { database } from '@/app/db';
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NODE_ENV === 'production') {
-    console.log('Running migrations');
-    console.log(__dirname);
-    console.log(resolve(__dirname, 'drizzle'));
     await migrate(database, {
-      migrationsFolder: resolve(__dirname, 'drizzle'),
+      migrationsFolder: 'drizzle',
       migrationsSchema: 'public',
     });
   }
