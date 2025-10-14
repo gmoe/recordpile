ba0fce142e36bdc50849dbd55daacf932d8adcaa
Griffin Moe <me@griffinmoe.com>
2025-10-13 20:35:22 -0500

    logging

--- app/instrumentation.ts
+++ app/instrumentation.ts
@@ -1,11 +1,15 @@
+import { resolve } from 'node:path';
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 
 import { database } from '@/app/db';
 
 export async function register() {
   if (process.env.NEXT_RUNTIME === 'nodejs' && process.env.NODE_ENV === 'production') {
+    console.log('Running migrations');
+    console.log(__dirname);
+    console.log(resolve(__dirname, 'drizzle'));
     await migrate(database, {
-      migrationsFolder: './drizzle',
+      migrationsFolder: resolve(__dirname, 'drizzle'),
       migrationsSchema: 'public',
     });
   }
